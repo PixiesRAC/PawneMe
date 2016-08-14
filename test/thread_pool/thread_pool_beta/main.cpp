40a937e1e61042e199de8444b55faf632c0df67f
pixies <delarossi77@gmail.com>
2016-08-14 17:35:50 +0200

    Ajout thread pool beta dans test

---
+++ test/thread_pool/thread_pool_beta/main.cpp
@@ -0,0 +1,101 @@
+#include <future>
+#include <thread>
+#include <condition_variable>
+#include <mutex>
+#include <iostream>
+#include <vector>
+#include <queue>
+
+void	fct_thread()
+{
+  std::this_thread::sleep_for (std::chrono::seconds(3));
+  std::cout << "Je suis dans ma fonction thread" << std::endl;
+}
+
+class	threadPool
+{
+  threadPool(threadPool&) = delete;
+  threadPool& operator=(threadPool&) = delete;
+  
+public :
+  
+  threadPool(int size);
+  ~threadPool();
+
+  /* Ajoute les taches a la queue */
+  void	addTask(std::function<void()>);
+
+private :
+
+  /* Stock les threads */
+  std::vector<std::thread>		_vThread;
+
+  /* Stock les taches, en gros la taches de la fonction utilisée */
+  std::queue<std::function<void()>>	_qTasks;
+
+  std::condition_variable	       	_cond_var;
+  std::mutex				_mutex;
+
+  bool					done;
+  void					fct_call();
+};
+
+threadPool::threadPool(int size) {
+
+  while (size != 0) {
+    this->_vThread.push_back(std::thread(&threadPool::fct_call, this));
+    --size;
+  }
+}
+
+threadPool::~threadPool() {
+  for (auto &thread : _vThread) {
+    if (thread.joinable()) {
+      thread.join();
+    }
+  }
+}
+
+void threadPool::fct_call() {
+
+  std::function<void()>	fctTask;
+
+  std::unique_lock<std::mutex>	verrou(_mutex);
+
+  std::cout << "Je suis en train d'attendre le lancement des threads !" << std::endl;
+  _cond_var.wait(verrou,
+		 [this]
+		 {
+		   return (!_qTasks.empty());});
+
+  if (_qTasks.empty()) {
+    return;
+  }
+		       
+  std::cout << "J'ai fini d'attendre" << std::endl;
+  fctTask = this->_qTasks.front();
+  _qTasks.pop();
+  fctTask();
+}
+
+void	threadPool::addTask(std::function<void()> fct) {
+  
+  std::unique_lock<std::mutex>	verrou(_mutex);
+  _qTasks.push(fct);
+  _cond_var.notify_one();
+}
+
+int	main()
+{
+
+  threadPool	tP(3);
+
+  tP.addTask(fct_thread);
+  for (int i = 0; i != 100000; ++i) {
+    std::cout << "Deroulement du jeux" << std::endl;
+  }
+  tP.addTask(fct_thread);
+  tP.addTask(fct_thread);
+  tP.addTask(fct_thread);
+  return (0);
+}
