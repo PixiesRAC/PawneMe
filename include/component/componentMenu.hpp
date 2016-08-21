b6578f3f27de453e8b54e4ba73dd4bd8e3c44602
pixies <pixies@localhost.localdomain>
2016-08-21 05:20:28 +0200

    Recode de la fonction contain de la sfml ajout de divers structure/enum necessaire pour la fonction + commentaires

--- include/component/componentMenu.hpp
+++ include/component/componentMenu.hpp
@@ -4,6 +4,47 @@
 #include "../engine/engine_component.hpp"
 #include "../component/componentWindow.hpp"
 
+/* Obliger de le faire sois meme ... fucking sfml */
+
+/** 
+ * \enum e_infoSprite
+ * \brief enum contenant le type de sprite (Utile pour savoir si le jouer choisis de jouer ou de load ou de quitter ou n'importe quoi)
+ * \author pixies
+ */
+
+typedef enum class e_infoSprite {
+  USELESS,
+    PLAY,
+    STOP
+
+    }		t_infoSprite;
+
+
+/** 
+ * \struct e_RectCoorSprite
+ * \brief struct recode de sf::Rect pour les int (à refaire vite fait avec les templates) de la sfml avec une information suplementaire sur le sprites en question 
+ * \author pixies
+ */
+
+typedef struct	s_RectCoorSprite {
+  int		rectLeft; /* Left coordinate of the rectangle */
+  int		rectTop; /* Top coordinate of the rectangle */
+  int		rectWidth; /* Width of the rectangle */
+  int		rectHeight; /* Height of the rectangle */
+  t_infoSprite	who; /* Valeur du sprite */
+}		e_RectCoorSprite;
+
+/** 
+ * \class t_InfSprite
+ * \brief struct  Permet d'avoir une information suplementaire par rapport au sprite mit
+ * \author pixies
+ */
+
+typedef struct	s_InfSprite
+{
+  sf::Sprite	sprite;
+  t_infoSprite	who;
+}		t_InfSprite;
 
 /** 
  * \class componentMenu
@@ -26,23 +67,66 @@ public :
   
 private :
 
+  /** 
+ * \fn createSprites
+ * \brief Crée les sprites et les push dans le vector de sprites
+ * \author pixies
+ */
+
   void		createSprites();
+
+  
+  /** 
+ * \fn fillRect
+ * \brief Remplie le vecteur de structure e_RectCoorSprite avec tout les sprites ajouté
+ * \author pixies
+ */
+
+  void		fillRect();
+
+  /** 
+ * \fn drawSprites
+ * \brief affiche les sprites du vecteur
+ * \author pixies
+ */
+  
   void		drawSprites();
+
+
+  /** 
+ * \fn keyboardMouseMenu
+ * \brief event clavier ou souris
+ * \author pixies
+ */
+  
   void		keyboardMouseMenu();
-  bool		isContainsMySprites(int, int);
+
+  /** 
+ * \fn isContainMySprites
+ * \brief Fait une rectangulation des coordonée fournis avec les coordoné situé dans le vector de e_RectCoorSprite (recode de la fonction contain de la sfml)
+ * \param int x coordonnée X pointé par la souris int y coordonnéé y pointé par la souris
+ * \return true si un poin a été trouver, false sinon
+ * \author pixies
+ */
+
+  bool		isContainsMySprites(int x , int y);
 
   /* Get window from componentWindow STATIC */
   
-  sf::RenderWindow&	_Cwindow;
+  sf::RenderWindow&		_Cwindow;
 
   /* Event */
   
   sf::Event			_event;
 
-  std::vector<sf::Sprite>	_sprites;
+  /* vector de sprite */
+  std::vector<t_InfSprite>	_sprites;
+
+  /* vector de rectangle de sprite */
+  std::vector<e_RectCoorSprite>	_vCoorSprites;
   
   sf::Texture			_textureMenu;
-  sf::Sprite			_spriteMenu;
+  t_InfSprite			_spriteMenu;
 
   /* A voir si on garde cette methode pour les texture */
 
@@ -53,7 +137,7 @@ private :
   /* http://www.sfml-dev.org/tutorials/2.4/graphics-sprite-fr.php le moin de texture possible A VOIR */
   /* http://www.sfml-dev.org/documentation/2.4.0-fr/classsf_1_1Texture.php */
   sf::Texture			_textureButtonPlay;
-  sf::Sprite			_spriteButtonPlay;
+  t_InfSprite			_spriteButtonPlay;
 
   sf::IntRect			_buttonArea;
 
--- src/component/componentMenu.cpp
+++ src/component/componentMenu.cpp
@@ -8,58 +8,77 @@ componentMenu::componentMenu() : _Cwindow(componentWindow::getWindow())
 {
   /* Permet d'adapter (mettre à l'echelles) la taille du sprites à l'ecran */
   
-  this->_spriteMenu.setScale(sf::Vector2f(sf::VideoMode::getDesktopMode().width / componentMenu::_LwindowsMenu, sf::VideoMode::getDesktopMode().height / componentMenu::_HwindowsMenu));
+  this->_spriteMenu.sprite.setScale(sf::Vector2f(sf::VideoMode::getDesktopMode().width / componentMenu::_LwindowsMenu, sf::VideoMode::getDesktopMode().height / componentMenu::_HwindowsMenu));
 
   /* Scale du button, prendra 15% du menu, le calcul c'est juste un pourcentage et ensuite une mise a l'echelle suite au resultat trouver via le pourcentage */
   
-  this->_spriteButtonPlay.setScale(sf::Vector2f((sf::VideoMode::getDesktopMode().width - (sf::VideoMode::getDesktopMode().width * componentMenu::_PourcentageButton / 100)) / componentMenu::_LwindowsMenuPlay,
+  this->_spriteButtonPlay.sprite.setScale(sf::Vector2f((sf::VideoMode::getDesktopMode().width - (sf::VideoMode::getDesktopMode().width * componentMenu::_PourcentageButton / 100)) / componentMenu::_LwindowsMenuPlay,
 						(sf::VideoMode::getDesktopMode().height - (sf::VideoMode::getDesktopMode().height * componentMenu::_PourcentageButton / 100)) / componentMenu::_HwindowsMenuPlay));
 
-  this->_spriteButtonPlay.setPosition((sf::VideoMode::getDesktopMode().width - (sf::VideoMode::getDesktopMode().width * 75 / 100))
+  this->_spriteButtonPlay.sprite.setPosition((sf::VideoMode::getDesktopMode().width - (sf::VideoMode::getDesktopMode().width * 75 / 100))
 				      , 0);
-
-  //  this->_buttonArea(this->_spriteButtonPlay.getPosition(), size);
 }
 
 void		 componentMenu::createSprites()
 {
+  /* MENU */
   if ((this->_textureMenu.loadFromFile(componentMenu::_fileTextureMenu)) == true) {
-    this->_spriteMenu.setTexture(this->_textureMenu);
+    this->_spriteMenu.sprite.setTexture(this->_textureMenu);
+    this->_spriteMenu.who = t_infoSprite::USELESS;
   }
+  /* PLAY */
   if ((this->_imgButtonPlay.loadFromFile(componentMenu::_fileTextureMenuPlay)) == true) {
     this->_imgButtonPlay.createMaskFromColor(sf::Color::Black);
     this->_textureButtonPlay.loadFromImage(this->_imgButtonPlay);
-    this->_spriteButtonPlay.setTexture(this->_textureButtonPlay);
+    this->_spriteButtonPlay.sprite.setTexture(this->_textureButtonPlay);
+    this->_spriteButtonPlay.who = t_infoSprite::PLAY;
   }
-  //  sf::IntRect lol(this->_spriteButtonPlay.getPosition(), sf::Vector2i(200,200));
+
+
   this->_sprites.push_back(this->_spriteMenu);
   this->_sprites.push_back(this->_spriteButtonPlay);
+  this->fillRect();
 }
 
+void		componentMenu::fillRect() {
+  for (auto &sprite : this->_sprites) {
+
+    if (sprite.who != t_infoSprite::USELESS) {
+      e_RectCoorSprite RectCoorSprite;
+    
+      RectCoorSprite.rectLeft = sprite.sprite.getPosition().x;
+      RectCoorSprite.rectTop = sprite.sprite.getPosition().y;
+      RectCoorSprite.rectWidth = sprite.sprite.getPosition().x + (componentMenu::_LwindowsMenuPlay * sprite.sprite.getScale().x);
+      RectCoorSprite.rectHeight = sprite.sprite.getPosition().y + (componentMenu::_HwindowsMenuPlay * sprite.sprite.getScale().y);
+      RectCoorSprite.who = sprite.who;
+      this->_vCoorSprites.push_back(RectCoorSprite);
+    }
+  }
+}
+  
 void		 componentMenu::drawSprites()
 {
   for (auto &sprite : this->_sprites) {
-    this->_Cwindow.draw(sprite);
+    this->_Cwindow.draw(sprite.sprite);
   }
 }
 
 bool		componentMenu::isContainsMySprites(int x, int y) {
-  
-  const short		xStart = this->_spriteButtonPlay.getPosition().x;
-  const short		xEnd = this->_spriteButtonPlay.getPosition().x + (componentMenu::_LwindowsMenuPlay * this->_spriteButtonPlay.getScale().x);
-  const short		yStart = this->_spriteButtonPlay.getPosition().y;
-  const short		yEnd = this->_spriteButtonPlay.getPosition().y + (componentMenu::_HwindowsMenuPlay * this->_spriteButtonPlay.getScale().y);
-  
-  if (x >= xStart && x <= xEnd && y >= yStart && y <= yEnd) {
-    return (true);
+
+  for (auto &rect : this->_vCoorSprites) {
+    if (x >= rect.rectLeft && x <= rect.rectWidth && y >= rect.rectTop && y <= rect.rectHeight) {
+      if (rect.who == t_infoSprite::PLAY) { /* Go faire une map, c'est mieux je pense */
+	std::cout << "On vient de detecter que le joueur veut jouer" << std::endl;
+	/* GO JOUER */
+      }
+      return (true);
+    }
   }
   return (false);
 }
 
 void             componentMenu::keyboardMouseMenu()
 {
-  //  static int x = 0;
-  //  static int y = 0;
   if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
     {
       if (this->isContainsMySprites(sf::Mouse::getPosition(this->_Cwindow).x, sf::Mouse::getPosition(this->_Cwindow).y)) {
